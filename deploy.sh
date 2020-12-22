dedb40c274347b300aea3a4ddf0d68af2e7ef498
hatlonely <hatlonely@gmail.com>
2020-12-23 01:54:32 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,17 @@ function Warn() {
     return 1
 }
 
+function CheckNotEmpty() {
+    [ -n "$(eval echo "\$$1")" ] || Warn "$1 is empty"
+}
+
 function Build() {
+    CheckNotEmpty "REGISTRY_USERNAME" || return 1
+    CheckNotEmpty "REGISTRY_PASSWORD" || return 1
+    CheckNotEmpty "REGISTRY_SERVER" || return 1
+    CheckNotEmpty "REGISTRY_NAMESPACE" || return 1
+    CheckNotEmpty "IMAGE_REPOSITORY" || return 1
+    CheckNotEmpty "IMAGE_TAG" || return 1
     cd .. && make image && cd - || return
     docker login --username="${REGISTRY_USERNAME}" --password="${REGISTRY_PASSWORD}" "${REGISTRY_SERVER}"
     docker tag "${REGISTRY_NAMESPACE}/${IMAGE_REPOSITORY}:${IMAGE_TAG}" "${REGISTRY_SERVER}/${REGISTRY_NAMESPACE}/${IMAGE_REPOSITORY}:${IMAGE_TAG}"
@@ -21,12 +31,16 @@ function Build() {
 }
 
 function SQLTpl() {
+    CheckNotEmpty "NAMESPACE" || return 1
+    CheckNotEmpty "MYSQL_SERVER" || return 1
+    CheckNotEmpty "MYSQL_ROOT_PASSWORD" || return 1
     environment=$1
     kubectl run -n "${NAMESPACE}" -it --rm sql --image=mysql:5.7.30 --restart=Never -- \
       mysql -uroot -h"${MYSQL_SERVER}" -p"${MYSQL_ROOT_PASSWORD}" -e "$(cat "tmp/${environment}/create_table.sql")"
 }
 
 function CreateNamespaceIfNotExists() {
+    CheckNotEmpty "NAMESPACE" || return 1
     kubectl get namespaces "${NAMESPACE}" 2>/dev/null 1>&2 && return 0
     kubectl create namespace "${NAMESPACE}" &&
     Info "create namespace ${NAMESPACE} success" ||
@@ -34,6 +48,11 @@ function CreateNamespaceIfNotExists() {
 }
 
 function CreatePullSecretsIfNotExists() {
+    CheckNotEmpty "NAMESPACE" || return 1
+    CheckNotEmpty "PULL_SECRETS" || return 1
+    CheckNotEmpty "REGISTRY_SERVER" || return 1
+    CheckNotEmpty "REGISTRY_USERNAME" || return 1
+    CheckNotEmpty "REGISTRY_PASSWORD" || return 1
     CreateNamespaceIfNotExists || return 1
     kubectl get secret "${PULL_SECRETS}" -n "${NAMESPACE}" 2>/dev/null 1>&2 && return 0
     kubectl create secret docker-registry "${PULL_SECRETS}" \
@@ -58,6 +77,12 @@ EOF"
 }
 
 function Test() {
+    CheckNotEmpty "NAMESPACE" || return 1
+    CheckNotEmpty "NAME" || return 1
+    CheckNotEmpty "REGISTRY_SERVER" || return 1
+    CheckNotEmpty "REGISTRY_NAMESPACE" || return 1
+    CheckNotEmpty "IMAGE_REPOSITORY" || return 1
+    CheckNotEmpty "IMAGE_TAG" || return 1
     kubectl run -n "${NAMESPACE}" -it --rm "${NAME}" \
       --image="${REGISTRY_SERVER}/${REGISTRY_NAMESPACE}/${IMAGE_REPOSITORY}:${IMAGE_TAG}" \
       --restart=Never \
@@ -65,16 +90,21 @@ function Test() {
 }
 
 function AddLabel() {
+    CheckNotEmpty "NODE_AFFINITY_LABEL_KEY" || return 1
+    CheckNotEmpty "NODE_AFFINITY_LABEL_VAL" || return 1
     node=$1
     kubectl label node "${node}" "${NODE_AFFINITY_LABEL_KEY}=${NODE_AFFINITY_LABEL_VAL}" --overwrite=true
 }
 
 function DelLabel() {
+    CheckNotEmpty "NODE_AFFINITY_LABEL_KEY" || return 1
     node=$1
     kubectl label node "${node}" "${NODE_AFFINITY_LABEL_KEY}"-
 }
 
 function AddTaint() {
+    CheckNotEmpty "TOLERATIONS_TAINT_KEY" || return 1
+    CheckNotEmpty "TOLERATIONS_TAINT_VAL" || return 1
     node=$1
     kubectl taint node "${node}" "${TOLERATIONS_TAINT_KEY}=${TOLERATIONS_TAINT_VAL}:NoExecute" --overwrite=true
 }
@@ -85,25 +115,34 @@ function DelTaint() {
 }
 
 function Install() {
+    CheckNotEmpty "TOLERATIONS_TAINT_KEY" || return 1
     environment=$1
     helm install "${NAME}" -n "${NAMESPACE}" "tmp/${environment}/${NAME}" -f "tmp/${environment}/chart.yaml"
 }
 
 function Upgrade() {
+    CheckNotEmpty "NAMESPACE" || return 1
+    CheckNotEmpty "NAME" || return 1
     environment=$1
     helm upgrade "${NAME}" -n "${NAMESPACE}" "tmp/${environment}/${NAME}" -f "tmp/${environment}/chart.yaml"
 }
 
 function Diff() {
+    CheckNotEmpty "NAMESPACE" || return 1
+    CheckNotEmpty "NAME" || return 1
     environment=$1
     helm diff upgrade "${NAME}" -n "${NAMESPACE}" "tmp/${environment}/${NAME}" -f "tmp/${environment}/chart.yaml"
 }
 
 function Delete() {
+    CheckNotEmpty "NAMESPACE" || return 1
+    CheckNotEmpty "NAME" || return 1
     helm delete "${NAME}" -n "${NAMESPACE}"
 }
 
 function Restart() {
+    CheckNotEmpty "NAMESPACE" || return 1
+    CheckNotEmpty "NAME" || return 1
     kubectl get pods -n "${NAMESPACE}" | grep "${NAME}" | awk '{print $1}' | xargs kubectl delete pods -n "${NAMESPACE}"
 }
 
